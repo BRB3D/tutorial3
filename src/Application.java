5a9ec83d049ad7ac8331175325c93dda6fe70080
BRB3D <samuraix221@hotmail.com>
2022-04-02 14:01:26 -0400

    Strings + concatenation

---
+++ src/Application.java
@@ -0,0 +1,14 @@
+public class Application {
+    public static void main(String[] args) {
+        int myInt = 7;
+
+        String text = "Hi there";
+        String blank = "    ";
+        String name = "David";
+        String greeting = text + blank + name;
+        System.out.println(text);
+        System.out.println(greeting);
+        System.out.println("Hello" + " " + "David");
+        System.out.println("My integer is: " + myInt);
+    }
+}
